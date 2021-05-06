c6127e70856c5c96feebccd3eab7078178b4306c
vinnyk <ragdolleye@gmail.com>
2021-05-06 14:22:48 -0500

    Adds comments regarding how it could be cleaned-up.

--- app/controllers/parameters_controller.rb
+++ app/controllers/parameters_controller.rb
@@ -2,11 +2,15 @@ class ParametersController < ApplicationController
 
   def name_game
     name = params[:name]
+    # have output_message here (this commented method is more efficient)
     if name.downcase.split(//).first == "a"
       render json: {message: "Hello, #{name.capitalize}, your name starts with the first letter of the alphabet."}
+      # have just an if statement to change the output message if true
     else
       render json: {message: "Hello, #{name.capitalize}."}
     end
+
+    # have render message down here with output_message
   end
   
   def guess_a_number
@@ -23,5 +27,7 @@ class ParametersController < ApplicationController
       render json: {message: "#{guess} is too high."}
     end
   end
-  
+  # same as above. have an output_message variable for each if and elsif
+  # then just render json: {message: output_message}
+  # do this clean-up later.
 end
