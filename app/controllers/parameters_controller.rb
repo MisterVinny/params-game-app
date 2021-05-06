b5833036a3b58360cef7542cd97771dd32d7eaed
vinnyk <ragdolleye@gmail.com>
2021-05-06 13:53:54 -0500

    Includes .to_i to assignment of guess variable and removes it from the instances in the IF statement.

--- app/controllers/parameters_controller.rb
+++ app/controllers/parameters_controller.rb
@@ -11,18 +11,17 @@ class ParametersController < ApplicationController
   
   def guess_a_number
     winning_number = 52
-    guess = params[:guess]
+    guess = params[:guess].to_i
 
-    if guess.to_i < 1 || guess.to_i > 100
+    if guess < 1 || guess > 100
       render json: {message: "The number has to be between 1 and 100."}
-    elsif guess.to_i == winning_number
+    elsif guess == winning_number
       render json: {message: "You win! The number was #{winning_number}"}
-    elsif guess.to_i < winning_number
+    elsif guess < winning_number
       render json: {message: "#{guess} is too low."}
-    elsif guess.to_i > winning_number
+    elsif guess > winning_number
       render json: {message: "#{guess} is too high."}
     end
   end
   
-
 end
